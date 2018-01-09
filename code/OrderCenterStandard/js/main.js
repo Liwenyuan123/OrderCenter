375deb78c44e87e29e8b989e12b79c45dc4b40a8
ShiFen <1018497215@qq.com>
2018-01-09 11:16:21 +0800

    修改登录

--- code/OrderCenterStandard/js/main.js
+++ code/OrderCenterStandard/js/main.js
@@ -4,15 +4,13 @@
     $("#mainIframe").height($(window).height() - $("#hd").outerHeight());
    
     $('.munLi').click(function () {
-        var munUrl = $(this).attr('munUrl');
-        //old
-        var oldElement = $(this).siblings().find('.changeColor:eq(0)');
-        console.log(typeof oldElement);
-        oldElement.children('span:eq(0)').children().find('img:eq(1)').remove();
+        var munUrl = $(this).attr('munUrl');        
+        $(this).parent().find('.changeColor:eq(0)').children('span:eq(0)').find('img:eq(1)').remove();
+        
         $(this).siblings().removeClass('changeColor');
         $(this).addClass('changeColor');
         $(this).find('span:eq(0)').append('<img class="current" src="../image/current.png" />');
-        $(this).siblings().children().find('span:eq(0)').children().find('.current').remove();
+        
         $("#mainIframe").attr("src", munUrl);
     })
 })
